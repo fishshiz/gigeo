7070d7737b2e27117bae3d5070c3a1bf03f20fa1
Edan Lewis <edan@lewisarts.com>
2026-03-31 12:29:51 -0400

    fix ts

--- apps/web/src/EventFilter.tsx
+++ apps/web/src/EventFilter.tsx
@@ -7,27 +7,27 @@ const EventFilter = () => {
   const classifications = Object.values(events)
     .flat()
     .flatMap((e) => e.classifications || [])
-    .reduce((acc: string[], curr: Classification) => {
+    .reduce((acc: { id: string; name: string }[], curr: Classification) => {
       if (
         curr.primary &&
         curr.segment.name &&
-        !acc.includes(curr.segment.name)
+        !acc.find((item) => item.name === curr.segment.name)
       ) {
-        acc.push(curr.segment.name)
+        acc.push({ id: curr.segment.name, name: curr.segment.name })
       }
       return acc
     }, [])
   console.log(classifications)
   return (
-    <TagGroup items={classifications}>
+    <TagGroup>
       <TagList className="gap 2 flex flex-wrap" items={classifications}>
         {classifications.map((classification) => (
           <Tag
-            key={classification}
-            id={classification}
+            key={classification.id}
+            id={classification.id}
             className="rounded-full bg-gray-200 px-2 py-1 text-sm text-gray-700"
           >
-            {classification}
+            {classification.name}
           </Tag>
         ))}
       </TagList>
