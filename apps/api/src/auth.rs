9872da7686a51748de31655cb5bb1665b3883848
Edan Lewis <edan@lewisarts.com>
2026-06-29 16:04:59 -0400

    update auth url

--- apps/api/src/auth.rs
+++ apps/api/src/auth.rs
@@ -195,7 +195,7 @@ impl UserTokenManager {
     pub fn authorize_url(&self, state: &str) -> String {
         let scopes = "playlist-modify-public playlist-modify-private user-read-private";
         format!(
-            "https://accounts.spotify.com/authorize?response_type=code\
+            "https://accounts.spotify.com/authorize?response_type=code&show_dialog=true\
              &client_id={client_id}\
              &scope={scopes}\
              &redirect_uri={redirect_uri}\
